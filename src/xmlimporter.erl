d066300ffdcc6b0c1aa24b106a12fe7c018a899c
softberries <grajek_k@fnb.co.za>
2012-09-19 22:23:44 +0200

    nice try with records but doens't work

--- src/xmlimporter.erl
+++ src/xmlimporter.erl
@@ -2,10 +2,12 @@
 -module(xmlimporter).
 -author("kris").
 
+-import(queue,[in/1,out/1,new/0]).
 %% user interface
 -export([start/0]).
 
--record(artist,{id="",name="",url="",image="", mbgid=""}).
+-record(location,{country="",state="",city="",latitude="",longitude=""}).
+-record(artist,{id="",name="",url="",image="", mbgid="", location=#location{}}).
 
 %start() -> run("/Users/kris/Downloads/dbdump_artistalbumtrack.0.290905586176.xml",0).
 start() -> run("/Users/kris/Documents/projects/erlang_projects/playground/src/testsmall.xml",0).
@@ -21,38 +23,50 @@ run(File, Result) ->
 
 callback(Event, Acc) -> processTag(Event,Acc).
 
-processTag({startElement,[],"artist",[],[]},Acc) -> processStartArtist(Acc);
-processTag({startElement,[],"id",[],[]},Acc) -> processProperty(hd(Acc),id, element(1,hd(Acc)));
-processTag({endElement,[],"id",[]},Acc)->processProperty(hd(Acc),undefined, element(1,hd(Acc)));
-processTag({startElement,[],"name",[],[]},Acc) -> processProperty(hd(Acc),name, element(1,hd(Acc)));
-processTag({endElement,[],"name",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
-processTag({startElement,[],"url",[],[]},Acc) -> processProperty(hd(Acc),url, element(1,hd(Acc)));
-processTag({endElement,[],"url",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
-processTag({startElement,[],"mbgid",[],[]},Acc) -> processProperty(hd(Acc),mbgid, element(1,hd(Acc)));
-processTag({endElement,[],"mbgid",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
-processTag({startElement,[],"image",[],[]},Acc) -> processProperty(hd(Acc),image, element(1,hd(Acc)));
-processTag({endElement,[],"image",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
-processTag({startElement,[],_,[],[]},Acc) -> Acc;
-processTag({characters,Data},Acc) -> processSingle(hd(Acc),Data,Acc);
+isValidTag(T) -> lists:member(T,
+    ["artist","id","name","image","url","mbgid","country","state","city","location","latitude","longitude"]
+).
 
-processTag({endElement,[],"artist",[]},Acc) -> processEndArtist(Acc);
+processTag({startElement,[],T,[],[]},Acc) ->
+  case isValidTag(T) of
+            true -> processStartTag(T,Acc);
+            _Else    -> Acc
+  end;
+
+processTag({endElement,[],T,[]},Acc) ->
+  case isValidTag(T) of
+            true -> processEndTag(T,Acc);
+            _Else    -> Acc
+  end;
+
+processTag({characters,Data},Acc) -> [Path,Artist] = Acc, io:format("Path: ~p, Data: ~p~n",[Path,Data]),
+  case Path of {["artist"],[]} -> Acc;
+               {["id"],["artist"]} -> [Path,Artist#artist{id=Data}];
+               {["name"],["artist"]} -> [Path,Artist#artist{name=Data}];
+               {["url"],["artist"]} -> [Path,Artist#artist{url=Data}];
+               {["image"],["artist"]} -> [Path,Artist#artist{image=Data}];
+               {["mbgid"],["artist"]} -> [Path,Artist#artist{mbgid=Data}];
+               {["city","location"],["artist"]} ->  [Path,Artist#artist{location=#location{city=Data}}];
+               {["state","location"],["artist"]} -> [Path,Artist#artist{location=#location{state=Data}}];
+               {["country","location"],["artist"]} -> [Path,Artist#artist{location=#location{country=Data}}];
+               {["latitude","location"],["artist"]} -> [Path,Artist#artist{location=#location{latitude=Data}}];
+               {["longitude","location"],["artist"]} -> [Path,Artist#artist{location=#location{longitude=Data}}];
+    _Else    -> Acc
+  end;
+
 processTag(_,ok) -> [];
 processTag(_,Acc) -> Acc.
 
 
-processProperty({E,_},Property,Element) -> [{E,Property},Element].
+processStartTag("artist", _) -> Path = queue:new(), [queue:in("artist",Path),#artist{}];
+processStartTag(T,Acc) -> [Path,Artist] = Acc, [queue:in(T,Path),Artist].
 
-processSingle({E,id},Data, Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{id=Data},undefined},A];
-processSingle({E,name},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{name=Data},undefined},A];
-processSingle({E,url},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{url=Data},undefined},A];
-processSingle({E,mbgid},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{mbgid=Data},undefined},A];
-processSingle({E,image},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{image=Data},undefined},A];
-processSingle({_,undefined},_,Acc) -> Acc.
+processEndTag("artist",Acc) -> io:format("~p~n",[Acc]), [];
+processEndTag(_,Acc) -> [Path,Artist] = Acc, {_,Queue} = queue:out_r(Path), [Queue, Artist].
 
-%% Artist processing
 
-processStartArtist(_) -> io:format("~p~n",["start artist"]), E = #artist{id="",name="",url="",mbgid=""}, [{E,undefined},E].
-processEndArtist(Acc) -> [_,ToPrint] = Acc, io:format("~p ~p~n",["end artist: ", ToPrint]), [].
+
+
 
 %% this is just to make it easier to test this little example
 xml(File) -> filename:join([codeDir(), File]).
