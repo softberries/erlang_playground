7d8b4864e8e7b283c1d3d905bef80521003481a5
softberries <grajek_k@fnb.co.za>
2012-09-19 15:56:38 +0200

    getting basic artist info works

--- src/xmlimporter.erl
+++ src/xmlimporter.erl
@@ -5,9 +5,10 @@
 %% user interface
 -export([start/0]).
 
--record(artist,{id="",name="",url="",mbgid=""}).
+-record(artist,{id="",name="",url="",image="", mbgid=""}).
 
-start() -> run("/Users/kris/Downloads/dbdump_artistalbumtrack.0.290905586176.xml",0).
+%start() -> run("/Users/kris/Downloads/dbdump_artistalbumtrack.0.290905586176.xml",0).
+start() -> run("/Users/kris/Documents/projects/erlang_projects/playground/src/testsmall.xml",0).
 
 run(File, Result) ->
   case file:read_file(xml(File)) of
@@ -21,24 +22,38 @@ run(File, Result) ->
 callback(Event, Acc) -> processTag(Event,Acc).
 
 processTag({startElement,[],"artist",[],[]},Acc) -> processStartArtist(Acc);
-processTag({startElement,[],"id",[],[]},Acc) -> [{<<"id">>}] ++ Acc;
-processTag({startElement,[],"name",[],[]},Acc) -> [{<<"name">>}] ++ Acc;
-processTag({startElement,[],"url",[],[]},Acc) -> [{<<"url">>}] ++ Acc;
-processTag({startElement,[],"mbgid",[],[]},Acc) -> [{<<"mbgid">>}] ++ Acc;
-
-processTag({characters,Data},[]) -> [];
-processTag({characters,Data},Acc) -> Val = {element(1,hd(Acc)),Data}, [Val] ++ lists:nthtail(1,Acc);
+processTag({startElement,[],"id",[],[]},Acc) -> processProperty(hd(Acc),id, element(1,hd(Acc)));
+processTag({endElement,[],"id",[]},Acc)->processProperty(hd(Acc),undefined, element(1,hd(Acc)));
+processTag({startElement,[],"name",[],[]},Acc) -> processProperty(hd(Acc),name, element(1,hd(Acc)));
+processTag({endElement,[],"name",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
+processTag({startElement,[],"url",[],[]},Acc) -> processProperty(hd(Acc),url, element(1,hd(Acc)));
+processTag({endElement,[],"url",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
+processTag({startElement,[],"mbgid",[],[]},Acc) -> processProperty(hd(Acc),mbgid, element(1,hd(Acc)));
+processTag({endElement,[],"mbgid",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
+processTag({startElement,[],"image",[],[]},Acc) -> processProperty(hd(Acc),image, element(1,hd(Acc)));
+processTag({endElement,[],"image",[]},Acc) -> processProperty(hd(Acc),undefined, element(1,hd(Acc)));
+processTag({startElement,[],_,[],[]},Acc) -> Acc;
+processTag({characters,Data},Acc) -> processSingle(hd(Acc),Data,Acc);
 
 processTag({endElement,[],"artist",[]},Acc) -> processEndArtist(Acc);
 processTag(_,ok) -> [];
 processTag(_,Acc) -> Acc.
 
 
+processProperty({E,_},Property,Element) -> [{E,Property},Element].
+
+processSingle({E,id},Data, Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{id=Data},undefined},A];
+processSingle({E,name},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{name=Data},undefined},A];
+processSingle({E,url},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{url=Data},undefined},A];
+processSingle({E,mbgid},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{mbgid=Data},undefined},A];
+processSingle({E,image},Data,Acc) when is_record(E,artist) -> [_,A] = Acc, [{E#artist{image=Data},undefined},A];
+processSingle({_,undefined},_,Acc) -> Acc.
 
 %% Artist processing
 
-processStartArtist(Acc) -> io:format("~p~n",["start artist"]), [].
-processEndArtist(Acc) -> io:format("~p ~p~n",["end artist: ", Acc]).
+processStartArtist(_) -> io:format("~p~n",["start artist"]), E = #artist{id="",name="",url="",mbgid=""}, [{E,undefined},E].
+processEndArtist(Acc) -> [_,ToPrint] = Acc, io:format("~p ~p~n",["end artist: ", ToPrint]), [].
+
 %% this is just to make it easier to test this little example
 xml(File) -> filename:join([codeDir(), File]).
 codeDir() -> filename:dirname(code:which(?MODULE)).
