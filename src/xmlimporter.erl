e2ed88d47b8aacf26224d6bd601d3fe3751602ef
softberries <grajek_k@fnb.co.za>
2012-09-24 21:41:06 +0200

    finished first verion of xml parser, it works but its crap

--- src/xmlimporter.erl
+++ src/xmlimporter.erl
@@ -26,15 +26,27 @@ run(File, Result) ->
 callback(Event, Acc) -> processTag(Event,Acc).
 
 isValidTag(T) -> lists:member(T,
-    ["artist","id","name","image","url","mbgid","country","state","city","location","latitude","longitude","album","Albums","id3genre","licence_artwork","releasedate","filename","Tracks", "track","license"]
+    ["artist","id","name","image","url","mbgid","country","state","city","location","latitude","longitude","album","Albums","id3genre","licence_artwork","releasedate","filename","Tracks", "track","license", "Tags", "tag","idstr","weight"]
 ).
 
 addTracks(Path,AlbumNumber,TrackNumber, TagNumber, Artist) ->
-  io:format("Tracks started ~p, AlbumNumber: ~p~n",[Path,AlbumNumber]),
+%  io:format("Tracks started ~p, AlbumNumber: ~p~n",[Path,AlbumNumber]),
   {_,Albums} = getByKeyFromArtist(Artist,<<"Albums">>),
   {_,Album} = lists:nth(AlbumNumber,Albums),
   Replaced = replaceElement(Albums,AlbumNumber,{<<"album">>,Album++[{<<"Tracks">>,[]}]}),
   NewArtist = lists:keyreplace(<<"Albums">>,1,Artist,{<<"Albums">>,Replaced}),
+  [Path,AlbumNumber,TrackNumber,TagNumber,NewArtist].
+
+addTags(Path,AlbumNumber,TrackNumber,TagNumber,Artist) ->
+%  io:format("Tags started ~p, AlbumNumber: ~p, TrackNumber: ~p~n",[Path,AlbumNumber,TrackNumber]),
+  {_,Albums} = getByKeyFromArtist(Artist,<<"Albums">>),
+  {_,Tracks} = getByKeyFromAlbum(Artist,<<"Tracks">>,AlbumNumber),
+  {_,Album} = lists:nth(AlbumNumber,Albums),
+  {_,Track} = getByKeyFromTracks(Tracks,TrackNumber),
+  TracksNew = replaceElement(Tracks,TrackNumber,{<<"track">>,Track++[{<<"Tags">>,[]}]}),
+  AlbumNew = lists:keyreplace(<<"Tracks">>,1,Album,{<<"Tracks">>,TracksNew}),
+  AlbumsNew = replaceElement(Albums,AlbumNumber,{<<"album">>,AlbumNew}),
+  NewArtist = lists:keyreplace(<<"Albums">>,1,Artist,{<<"Albums">>,AlbumsNew}),
   [Path,AlbumNumber,TrackNumber,TagNumber,NewArtist].
 
 processTag({startElement,[],"location",[],[]},Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Artist] = Acc,
@@ -61,8 +73,32 @@ processTag({startElement,[],"track",[],[]},Acc) -> [Path,AlbumNumber,TrackNumber
 %  io:format("ARTIST: ~p~n",[NewArtist]),
   processStartTag("track",[Path,AlbumNumber,TrackNumber+1,TagNumber,NewArtist]);
 
+processTag({startElement,[],"tag",[],[]},Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Artist] = Acc,
+  {_,Albums} = getByKeyFromArtist(Artist,<<"Albums">>),
+%  io:format("Albums Tag: ~p~n",[Albums]),
+  {_,Tracks} = getByKeyFromAlbum(Artist,<<"Tracks">>,AlbumNumber),
+%  io:format("Tracks: ~p~n",[Tracks]),
+  {_,Album} = lists:nth(AlbumNumber,Albums),
+%  io:format("Album Tag: ~p~n",[Album]),
+  {_,Track} = lists:nth(TrackNumber,Tracks),
+%  io:format("Track: ~p~n",[Track]),
+  {_,Tags} = lists:keyfind(<<"Tags">>, 1, Track),
+%  io:format("Tags: ~p~n",[Tags]),
+  TrackNew = lists:keyreplace(<<"Tags">>,1,Track,{<<"Tags">>,Tags++[{<<"tag">>,[]}]}),
+%  io:format("TrackNew: ~p~n",[TrackNew]),
+  TracksNew = replaceElement(Tracks,TrackNumber,{<<"track">>,TrackNew}),
+%  io:format("TRACKS_NEW: ~p~n",[TracksNew]),
+  AlbumNew = lists:keyreplace(<<"Tracks">>,1,Album,{<<"Tracks">>,TracksNew}),
+%  io:format("AlbumNew: ~p~n",[AlbumNew]),
+  AlbumsNew = replaceElement(Albums,AlbumNumber,{<<"album">>,AlbumNew}),
+%  io:format("ALBUMS_NEW: ~p~n",[AlbumsNew]),
+  NewArtist = lists:keyreplace(<<"Albums">>,1,Artist,{<<"Albums">>,AlbumsNew}),
+%  io:format("ARTIST: ~p~n",[NewArtist]),
+  processStartTag("tag",[Path,AlbumNumber,TrackNumber,TagNumber+1,NewArtist]);
+
 
 processTag({startElement,[],"Tracks",[],[]},Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Artist] = Acc, processStartTag("Tracks",addTracks(Path,AlbumNumber,TrackNumber, TagNumber,Artist));
+processTag({startElement,[],"Tags",[],[]},Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Artist] = Acc, processStartTag("Tags",addTags(Path,AlbumNumber,TrackNumber, TagNumber,Artist));
 
 processTag({startElement,[],T,[],[]},Acc) ->
   case isValidTag(T) of
@@ -77,7 +113,7 @@ processTag({endElement,[],T,[]},Acc) ->
   end;
 
 processTag({characters,Data},Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Artist] = Acc,
-  io:format("AlbumNr: ~p, TrackNr: ~p, TagNr: ~p, Path: ~p, Data: ~p~n",[AlbumNumber,TrackNumber,TagNumber,Path,Data]),
+%  io:format("AlbumNr: ~p, TrackNr: ~p, TagNr: ~p, Path: ~p, Data: ~p~n",[AlbumNumber,TrackNumber,TagNumber,Path,Data]),
   case Path of
     %Artist
                {["artist"],[]} -> Acc;
@@ -109,6 +145,9 @@ processTag({characters,Data},Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Art
                {["numalbum","track","Tracks","album","Albums"],["artist"]} -> setTrackProperty(Path,Artist,Data,AlbumNumber,TrackNumber,TagNumber,<<"numalbum">>);
                {["id3genre","track","Tracks","album","Albums"],["artist"]} -> setTrackProperty(Path,Artist,Data,AlbumNumber,TrackNumber,TagNumber,<<"id3genre">>);
                {["license","track","Tracks","album","Albums"],["artist"]} -> setTrackProperty(Path,Artist,Data,AlbumNumber,TrackNumber,TagNumber,<<"license">>);
+    %Tags
+               {["idstr","tag","Tags","track","Tracks","album","Albums"],["artist"]} -> setTagProperty(Path,Artist,Data,AlbumNumber,TrackNumber,TagNumber,<<"idstr">>);
+               {["weight","tag","Tags","track","Tracks","album","Albums"],["artist"]} -> setTagProperty(Path,Artist,Data,AlbumNumber,TrackNumber,TagNumber,<<"weight">>);
     _Else    -> Acc
   end;
 
@@ -122,6 +161,7 @@ processStartTag(T,Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Artist] = Acc,
 processEndTag("artist",Acc) -> io:format("~p~n",[Acc]), [];
 processEndTag("Albums",Acc) -> [Path,_,_,_,Artist] = Acc, {_,Queue} = queue:out_r(Path), [ Queue, 0,0,0, Artist];
 processEndTag("Tracks",Acc) -> [Path,AlbumNumber,_,_,Artist] = Acc, {_,Queue} = queue:out_r(Path), [ Queue, AlbumNumber,0,0, Artist];
+processEndTag("Tags",Acc) -> [Path,AlbumNumber,TrackNumber,_,Artist] = Acc, {_,Queue} = queue:out_r(Path), [ Queue, AlbumNumber,TrackNumber,0, Artist];
 processEndTag("location",Acc) -> [Path,_,_,_,Artist] = Acc, {_,Queue} = queue:out_r(Path), [Queue, 0,0,0, Artist];
 processEndTag(_,Acc) -> [Path,AlbumNumber,TrackNumber,TagNumber,Artist] = Acc, {_,Queue} = queue:out_r(Path), [Queue, AlbumNumber,TrackNumber,TagNumber, Artist].
 
@@ -149,6 +189,31 @@ setTrackProperty(Path,Artist,Data,AlbumNumber,TrackNumber,TagNumber,Property) ->
   NewArtist = lists:keyreplace(<<"Albums">>,1,Artist,{<<"Albums">>,AlbumsNew}),
   [Path,AlbumNumber,TrackNumber,TagNumber,NewArtist].
 
+setTagProperty(Path,Artist,Data,AlbumNumber,TrackNumber,TagNumber,Property) -> T = {Property, Data},
+  io:format("setting tagNr: ~p, tagName: ~p, tagValue: ~p~n",[TagNumber,Property,Data]),
+  {_,Albums} = getByKeyFromArtist(Artist,<<"Albums">>),
+%  io:format("ALBUMS: ~p~n",[Albums]),
+  {_,Tracks} = getByKeyFromAlbum(Artist,<<"Tracks">>,AlbumNumber),
+%  io:format("TRACKS: ~p~n",[Tracks]),
+  {_,Album} = lists:nth(AlbumNumber,Albums),
+%  io:format("Album: ~p~n",[Album]),
+  {_,Track} = getByKeyFromTracks(Tracks,TrackNumber),
+  {_,Tags} = getByKeyFromTrack(Track,<<"Tags">>),
+  io:format("Tags: ~p~n",[Tags]),
+  {_,Tag} = lists:nth(TagNumber,Tags),
+  io:format("Tag: ~p~n",[Tag]),
+  TagsNew = replaceElement(Tags,TagNumber,{<<"tag">>,Tag++[T]}),
+  io:format("Tagnew: ~p~n",[TagsNew]),
+  TrackNew = lists:keyreplace(<<"Tags">>,1,Track,{<<"Tags">>,TagsNew}),
+  io:format("Track New: ~p~n",[TrackNew]),
+  TracksNew = replaceElement(Tracks,TrackNumber,{<<"track">>,TrackNew}),
+  io:format("Tracks New: ~p~n",[TracksNew]),
+  AlbumNew = lists:keyreplace(<<"Tracks">>,1,Album,{<<"Tracks">>,TracksNew}),
+  io:format("ALBUM NEW: ~p~n",[AlbumNew]),
+  AlbumsNew = replaceElement(Albums,AlbumNumber,{<<"album">>,AlbumNew}),
+  NewArtist = lists:keyreplace(<<"Albums">>,1,Artist,{<<"Albums">>,AlbumsNew}),
+  [Path,AlbumNumber,TrackNumber,TagNumber,NewArtist].
+
 setLocationProperty(Path,Artist,Data,AlbumNumber,TrackNumber, TagNumber,Property) -> T = {Property,Data}, {Key,Loc} = getByKeyFromArtist(Artist,<<"location">>),
   [Path,AlbumNumber,TrackNumber,TagNumber,lists:keyreplace(Key,1,Artist,{Key,Loc++[T]})].
 
@@ -164,10 +229,14 @@ replaceElement(List,Position,Element) ->
 getByKeyFromArtist(Artist,Key) ->  lists:keyfind(Key, 1, Artist).
 
 getByKeyFromAlbum(Artist,Key,AlbumNumber) -> {_,Albums} = getByKeyFromArtist(Artist,<<"Albums">>),
+%  io:format("AlbumNumber: ~p, Albums: ~p",[AlbumNumber,Albums]),
   {_,Album} = lists:nth(AlbumNumber,Albums),
+%  io:format("Albumx: ~p, Key: ~p, AlbumNumber: ~p~n",[Album,Key,AlbumNumber]),
   lists:keyfind(Key, 1, Album).
 
 getByKeyFromTracks(Tracks,TrackNumber) -> lists:nth(TrackNumber,Tracks).
+
+getByKeyFromTrack(Track,Key) -> lists:keyfind(Key,1,Track).
 
 %% this is just to make it easier to test this little example
 xml(File) -> filename:join([codeDir(), File]).
